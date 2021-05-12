71d62115f8eaee356b5077dc483bd5b830456f71
Nebojša Draganić <nebojsa311@gmail.com>
2021-05-12 21:43:07 +0200

    css tweak

--- src/Info.jsx
+++ src/Info.jsx
@@ -5,7 +5,7 @@ function Info(props) {
     return(
         <div id="infoWrapper">
             <p id="choiceOne">YOU PICKED: {props.choice}</p>
-            <p id="choiceTwo">THE HOUSE PICKED: {props.cpuChoice}</p>
+            <p id="choiceTwo">HOUSE PICKED: {props.cpuChoice}</p>
             <button onClick={props.reset}>Reset score</button>
             <p id="result">{props.result}</p>
             <p id="score">SCORE: {props.score}</p>
